c9279f2edd2ae0195f626e5f4f42c919b0aaa4d4
Schmutterers-Schmiede <trashmail837857@gmail.com>
2023-12-04 23:25:59 +0100

    refactoring

--- AdventOfCode23.Day4/Day4_Part2.cs
+++ AdventOfCode23.Day4/Day4_Part2.cs
@@ -11,28 +11,28 @@ namespace AdventOfCode23.main
     public static class Day4_Part2
     {
         private static Dictionary<int, int> cards = new Dictionary<int, int>();
-        
+
         public static void Run()
         {
             StringBuilder path = new StringBuilder();
             path.Append(AppDomain.CurrentDomain.BaseDirectory);
             path.Append("../../../../AdventOfCode23.Day4/input2.txt");
             StreamReader sr = new StreamReader(path.ToString());
-            
+
             int i = 0;
-            while(!sr.EndOfStream)
+            while (!sr.EndOfStream)
             {
                 cards.Add(i + 1, CountMatches(sr.ReadLine()));
                 i++;
             }
-            
+
             int copyCount = 0;
-            foreach(var card in cards)
+            foreach (var card in cards)
             {
                 Console.WriteLine($"Processing card {card.Key,-3} with match count {card.Value,-2}");
                 copyCount += GetCopyCountForCard(card);
             }
-            
+
             Console.WriteLine();
             Console.WriteLine($"Original cards: {cards.Count()}");
             Console.WriteLine($"Copies: {copyCount}");
@@ -41,49 +41,44 @@ namespace AdventOfCode23.main
         }
         private static int CountMatches(string line)
         {
-            
-            var substringbuffer = Regex.Replace(Regex.Replace(line, "Card *[0-9]*: *", ""), @"\s+", " ").Split(" | ").ToList();
+            var substringbuffer = Regex.Replace(Regex.Replace(line, "Card *[0-9]*: *", ""), @"\s+", " ").Split(" | ");
             List<int> cardNumbers = new List<int>();
             List<int> winningNumbers = new List<int>();
             foreach (string number in substringbuffer.First().Split(" "))
-            {
                 cardNumbers.Add(int.Parse(number));
-            }
-            foreach (string number in substringbuffer.Last().Split(" "))
-            {
+
+            foreach (string number in substringbuffer.Last().Split(" "))            
                 winningNumbers.Add(int.Parse(number));
-            }
+            
             int matchCount = 0;
-            foreach (int number in cardNumbers)
-            {
-                if (winningNumbers.Contains(number)) matchCount++;
-            }
+            foreach (int number in cardNumbers)            
+                if (winningNumbers.Contains(number)) matchCount++;            
+
             return matchCount;
         }
-        
-        
+
+
         private static int GetCopyCountForCard(KeyValuePair<int, int> card)
-        {  
+        {
             List<KeyValuePair<int, int>> copies = GetCopiesForCard(card);
             int result = copies.Count;
-            foreach(var copy in copies)
-            {
-                result += GetCopyCountForCard(copy);
-            }
+
+            foreach (var copy in copies)            
+                result += GetCopyCountForCard(copy);    
+            
             return result;
         }
-        
+
 
         private static List<KeyValuePair<int, int>> GetCopiesForCard(KeyValuePair<int, int> card)
         {
             List<KeyValuePair<int, int>> result = new List<KeyValuePair<int, int>>();
             if (card.Value == 0) return result;
+
+            for (int i = card.Key + 1; i <= card.Key + card.Value; i++)           
+                result.Add(new KeyValuePair<int, int>(i, cards[i]));
             
-            for (int i = card.Key + 1; i <= card.Key + card.Value; i++)
-            {
-                result.Add(new KeyValuePair<int, int>(i, cards[i]));                
-            }            
             return result;
-        }        
+        }
     }
 }
